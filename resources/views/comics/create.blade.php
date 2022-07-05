ae15ff1ff1d6c61262a26be882def8cf0f8196e7
RenatoConfuorto <renato.confuorto@outlook.it>
2022-07-05 09:34:04 +0200

    Aggiunto valore old nella form create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -23,32 +23,32 @@
             <div class="form-group">
                 <label for="inputTitle">Titolo</label>
                 <input type="text" class="form-control" id="inputTitle" aria-describedby="emailHelp" placeholder="Titolo"
-                    name="title">
+                    name="title" value="{{ old('title') }}">
             </div>
             <div class="form-group">
                 <label for="inputDescription">Descrizione</label>
                 <input type="text" class="form-control" id="inputDescription" placeholder="Descrizione"
-                    name="description">
+                    name="description" value="{{ old('description') }}">
             </div>
             <div class="form-group">
                 <label for="inputThumb">Immagine</label>
-                <input type="text" class="form-control" id="inputThumb" placeholder="Immagine" name="thumb">
+                <input type="text" class="form-control" id="inputThumb" placeholder="Immagine" name="thumb" value="{{ old('thumb') }}">
             </div>
             <div class="form-group">
                 <label for="inputPrice">Prezzo</label>
-                <input type="text" class="form-control" id="inputPrice" placeholder="Prezzo" name="price">
+                <input type="text" class="form-control" id="inputPrice" placeholder="Prezzo" name="price" value="{{ old('price') }}">
             </div>
             <div class="form-group">
                 <label for="inputSeries">Serie</label>
-                <input type="text" class="form-control" id="inputSeries" placeholder="Serie" name="series">
+                <input type="text" class="form-control" id="inputSeries" placeholder="Serie" name="series" value="{{ old('series') }}">
             </div>
             <div class="form-group">
                 <label for="inputSaleDate">Data di uscita</label>
-                <input type="text" class="form-control" id="inputSaleDate" placeholder="Data di uscita" name="sale_date">
+                <input type="text" class="form-control" id="inputSaleDate" placeholder="Data di uscita" name="sale_date" value="{{ old('sale_date') }}">
             </div>
             <div class="form-group">
                 <label for="inputType">Tipo</label>
-                <input type="text" class="form-control" id="inputType" placeholder="Tipo" name="type">
+                <input type="text" class="form-control" id="inputType" placeholder="Tipo" name="type" value="{{ old('type') }}">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
